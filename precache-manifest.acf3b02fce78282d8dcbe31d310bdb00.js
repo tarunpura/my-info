ae645ddb0804c8384da563a4b33f4f972d5adb24
tarun pura <tarunpura@taruns-MacBook-Pro.local>
2024-02-12 15:42:37 -0500

    Updates

---
+++ precache-manifest.acf3b02fce78282d8dcbe31d310bdb00.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69f759fddd47ddddf691674ba34e2776",
+    "url": "/my-info/index.html"
+  },
+  {
+    "revision": "c17d814a985469b9217b",
+    "url": "/my-info/static/css/main.af970546.chunk.css"
+  },
+  {
+    "revision": "2797e4836420979bbda3",
+    "url": "/my-info/static/js/2.81cc2efc.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/my-info/static/js/2.81cc2efc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df2ccde89c8da173d90e",
+    "url": "/my-info/static/js/3.0d367ec3.chunk.js"
+  },
+  {
+    "revision": "c17d814a985469b9217b",
+    "url": "/my-info/static/js/main.85656edc.chunk.js"
+  },
+  {
+    "revision": "1a306e5320ba4df16bac",
+    "url": "/my-info/static/js/runtime-main.c83639e6.js"
+  }
+]);
